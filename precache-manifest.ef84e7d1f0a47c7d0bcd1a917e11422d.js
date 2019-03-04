ef018c197d92c85f5477d49f59193246ec1dc82a
Divya Nair <divnairs@gmail.com>
2019-03-03 16:07:27 -0800

    Updates

---
+++ precache-manifest.ef84e7d1f0a47c7d0bcd1a917e11422d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "581a7b7a36021c230303",
+    "url": "/google-keep-clone/static/css/main.b5f47fae.chunk.css"
+  },
+  {
+    "revision": "581a7b7a36021c230303",
+    "url": "/google-keep-clone/static/js/main.581a7b7a.chunk.js"
+  },
+  {
+    "revision": "6cf3d12ec703a2d2cd17",
+    "url": "/google-keep-clone/static/js/runtime~main.6cf3d12e.js"
+  },
+  {
+    "revision": "8704c04561cef13131e8",
+    "url": "/google-keep-clone/static/js/2.8704c045.chunk.js"
+  },
+  {
+    "revision": "6446b80158824c8e4801e8492b9c0610",
+    "url": "/google-keep-clone/index.html"
+  }
+];
